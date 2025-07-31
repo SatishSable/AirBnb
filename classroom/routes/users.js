977f210506147715f908485d9f9a6d1c55aec6e7
Satish Sable <satishsable2004@gmail.com>
2025-07-31 23:15:18 +0530

    cookies

--- classroom/routes/users.js
+++ classroom/routes/users.js
@@ -1,6 +1,6 @@
 
 const express = require("express");
-const router = ewxpress.Router();
+const router = express.Router();
 
 // users routes
 
--- classroom/server.js
+++ classroom/server.js
@@ -2,8 +2,31 @@ const express = require("express");
 const app = express();
 const users = require("./routes/users.js");
 const posts = require("./routes/posts.js");
+const cookiesParser = require("cookie-parser");
+const cookieParser = require("cookie-parser");
+app.use(cookieParser("yourSecret"));
+
+
+app.use(cookiesParser());
+
+app.get("/getsignedcookies" , (req ,res) => {
+    res.cookie("made-in" , "india" , {signed : true});
+    res.send("signed cookie set");
+})
+
+app.get("/getcookies" , (req ,res) => {
+    res.cookie("greet", "cookie");
+    res.cookie("madein", "india");
+    res.send("cookie, set");
+});
+
+app.get("/greet", (req ,res) => {
+    let {name = "Guest"} = req.cookies;     
+    res.send(`Hello ${name}`);
+})
 
 app.get("/" , (req ,res) => {
+    console.dir(req.cookies);
     res.send("hi , i am root ");
 });
 
