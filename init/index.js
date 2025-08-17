19f3fa11d334e00a04138708ee2e9ceb20ba9477
Satish Sable <satishsable2004@gmail.com>
2025-08-17 12:16:15 +0530

    to add navbar function

--- init/index.js
+++ init/index.js
@@ -21,7 +21,9 @@ async function main() {
 
 const initDB = async () => {
     await Listing.deleteMany({});
+   initData.data = initData.data.map((obj) => ({...obj , owner : "6892cd143e21b58834b6ee3f",}));
     await Listing.insertMany(initData.data);
+    
     console.log("data was initialized");
 };
 initDB();
