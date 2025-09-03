30b514ec61033f78135279a4d5d47ca664319f01
Satish Sable <satishsable2004@gmail.com>
2025-09-03 13:31:57 +0530

    to add marker

--- public/js/map.js
+++ public/js/map.js
@@ -5,6 +5,15 @@
     const map = new mapboxgl.Map({
       style : 'mapbox://styles/mapbox/streets-v11', // style URL
         container: 'map', // container ID
-        center: [ 76.2653,  20.3491], // starting position [lng, lat]. Note that lat must be set between -90 and 90
+        center: listing.geometry.coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
         zoom: 9 // starting zoom
     });
+
+
+    const marker = new mapboxgl.Marker({color : "red"})
+        .setLngLat(listing.geometry.coordinates) // Marker [lng, lat]
+        .setPopup(new mapboxgl.Popup({offset: 25})
+        .setHTML(
+            `<h3>${listing.title}</h3><p> Exect Location well be provided after booking</p>`
+        ))
+        .addTo(map);
