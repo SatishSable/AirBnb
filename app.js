d2874ef43e87e7d7187d58e7e63870158b1006f1
Satish Sable <satishsable2004@gmail.com>
2025-09-04 00:24:23 +0530

    to add search button

--- app.js
+++ app.js
@@ -62,9 +62,9 @@ const sessionOptions = {
 }
 
 // Home route
-app.get("/", (req, res) => {
-  res.send("Hi, I am route");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hi, I am route");
+// });
 
 
 
